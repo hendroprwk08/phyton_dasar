7c9ca305a215afe80f811f91c3fe0483ace312f8
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2020-12-26 16:32:57 +0700

    Initial commit

---
+++ dasar02-input.py
@@ -0,0 +1,4 @@
+nama = input('Hai, siapa namamu: ')
+usia = input('berapa usiamu: ')
+
+print(nama + ', kamu berusia '+ str(usia) +' tahun')
---
+++ dasar04-function.py
@@ -0,0 +1,15 @@
+#tanpa parameter
+def sayHai():
+    print('hai')
+
+#dengan parameter
+def haiKamu(n):
+    print('Aku bernama '+ n)
+
+#memiliki return
+def jumlah(a, b):
+    return print(str(a + b))
+
+sayHai()
+haiKamu('Iskandar')
+jumlah(45, 32)
